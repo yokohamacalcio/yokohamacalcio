5770af3c0f30b686ef069f10d2d4ea43b6e783a7
yokohamacalcio <yokohamacalcio@gmail.com>
2026-09-22 10:10:53 +0900

    Delete menu-toggle.js

--- js/menu-toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.mobile-menu-toggle');
-  const nav = document.querySelector('.main-nav');
-
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-});
